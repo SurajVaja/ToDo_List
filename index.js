36857f017980e2bc0f7cc434f8ee2469130c30ef
Suraj Vaja <surajvaja2324@gmail.com>
2023-09-13 17:16:54 +0530

    [Feature] : Added Fully CRUD

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let todo = [];
+let isEdit = "";
 
 let second = document.getElementById("second");
 let btn = document.getElementById("add");
@@ -10,7 +11,7 @@ let mapping = () => {
     second.innerHTML += `<div class="tex">
     <div class="tex-first"><h5>${item}</h5></div>
     <div class="tex-second">
-      <button type="button" class="btn btn-info grn">Edit</button>
+      <button type="button" class="btn btn-info grn" onclick='editToDo(${index})'>Edit</button>
       <button type="button" class="btn btn-info blu" onclick='deleteToDo(${index})'>X</button>
     </div>
   </div>`;
@@ -18,13 +19,29 @@ let mapping = () => {
 };
 
 let addToDo = () => {
-  let text = inputBox.value;
-  if (text != "") {
-    todo.push(text);
+  if (isEdit != "") {
+    let editId = Number(isEdit);
+    let newText = inputBox.value;
+
+    for (let i = 0; i < todo.length; i++) {
+      if (i == editId) {
+        todo[i] = newText;
+        break;
+      }
+    }
+
     mapping();
     inputBox.value = "";
+    btn.innerHTML = "Add";
   } else {
-    alert("Plese Enter some value");
+    let text = inputBox.value;
+    if (text != "") {
+      todo.push(text);
+      mapping();
+      inputBox.value = "";
+    } else {
+      alert("Plese Enter some value");
+    }
   }
 };
 btn.addEventListener("click", addToDo);
@@ -33,4 +50,12 @@ let deleteToDo = (id) => {
   let filteredArr = todo.filter((item, index) => index != id);
   todo = filteredArr;
   mapping();
+};
+
+let editToDo = (id) => {
+  isEdit = String(id);
+  btn.innerHTML = "Update";
+  let text = todo.find((item, index) => index == id);
+  inputBox.value = text;
+  inputBox.focus();
 };
